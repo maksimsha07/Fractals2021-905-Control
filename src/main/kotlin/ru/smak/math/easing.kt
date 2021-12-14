a9db028d79b3564bde6e0d2ec997646ac296b0d5
xavmag <chamaelion386@gmail.com>
2021-12-14 20:15:32 +0500

    Добавлено создание кадров для анимации

---
+++ src/main/kotlin/ru/smak/math/easing.kt
@@ -0,0 +1,17 @@
+package ru.smak.math
+
+import org.kotlinmath.Complex
+import kotlin.math.pow
+
+fun lerpUnclamped(p0: Complex, p1: Complex, t: Double): Complex {
+    return p0 + (p1 - p0) * t
+}
+
+fun lerpUnclamped(p0: Double, p1: Double, t: Double): Double {
+    return p0 + (p1 - p0) * t
+}
+
+fun easeOutExp(p0: Double, p1: Double, t: Double): Double {
+    val param = if (t == 1.0) 1.0 else -(2.0.pow(-10*t)) + 1
+    return p0 + (p1 - p0) * param
+}
--- src/main/kotlin/ru/smak/video/VideoRenderer.kt
+++ src/main/kotlin/ru/smak/video/VideoRenderer.kt
@@ -8,32 +8,29 @@ import org.kotlinmath.complex
 import ru.smak.events.Event
 import ru.smak.math.CatmullRom
 import ru.smak.math.complex.mod2
+import ru.smak.math.easeOutExp
 import ru.smak.ui.painting.CartesianPlane
 import java.awt.image.BufferedImage
 import java.lang.Double.min
 import kotlin.math.ln
+import kotlin.math.max
 
-/**
- * (Consumer) Класс, осуществляющий создание видео
- * @param frameWidth ширина кадра
- * @param frameHeight высота кадра
- * @param duration длительность видео
- * @param fps количество кадров в секунду
- */
-internal class VideoRenderer(
-    var frameWidth: Int = 1280, var frameHeight: Int = 720,
-    var duration: Int = 10, var fps: Int = 60
-) {
 
-    init {
-        onCreateVideoPressed.removeAll()
-        onCreateVideoPressed.add(this::createVideo)
-    }
+internal object VideoRenderer {
 
-    private fun createVideo(event: Event<CreateVideoData>, data: CreateVideoData) {
+    var frameWidth: Int = 1280
+    var frameHeight: Int = 720
+    var duration: Int = 10
+    var fps: Int = 60
 
+    val aspectRatio: Double
+    get() = frameWidth.toDouble() / frameHeight
+
+
+    fun createVideo(keyFrames: List<CartesianPlane>, fileName: String, formatType: String) {
+        val images = createFrameData(keyFrames)
         val frameDuration = Rational.make(1, fps)
-        val muxer = Muxer.make(data.fileName, null, data.formatType)
+        val muxer = Muxer.make(fileName, null, formatType)
         val format = muxer.format
         val codec = Codec.findEncodingCodec(format.defaultVideoCodecId)
 
@@ -59,7 +56,7 @@ internal class VideoRenderer(
         val packet = MediaPacket.make()
 
         repeat(fps * duration) { i ->
-            val img = BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB)//queue.take()
+            val img = BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB)//images[i]
             val newImg = BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB)
             newImg.graphics.drawImage(img, 0, 0, null)
             val image = convertToType(newImg, BufferedImage.TYPE_3BYTE_BGR)
@@ -75,38 +72,31 @@ internal class VideoRenderer(
     private fun createFrameData(keyFrames: List<CartesianPlane>): List<CartesianPlane> {
         val points = keyFrames.map { complex((it.xMin + it.xMax) * 0.5, (it.yMin + it.yMax) * 0.5) }
         val catmullRom = CatmullRom(points, 0.5)
+
         val v0 = keyFrames.first()
         val v1 = keyFrames.last()
         val finalZoom = min(v1.width.toDouble() / v0.width, v1.height.toDouble() / v0.height)
-        val finalLinZoom = ln(finalZoom)
-        val zoomStep = finalLinZoom / (fps * duration)
+
+        val width = max(v0.width, v0.height)
+        val height = width / aspectRatio
 
         val out = mutableListOf<CartesianPlane>()
-        repeat(fps * duration) {
-
+        val frameCount = fps * duration
+        repeat(frameCount) {
+            val t = it.toDouble() / (frameCount-1)
+            val zoom = easeOutExp(1.0, finalZoom, t)
+            val center = catmullRom(t)
+            val (x, y) = Pair(center.re, center.im)
+            out.add(CartesianPlane(
+                x - width * zoom * 0.5, x + width * zoom * 0.5,
+                y - height * zoom * 0.5, y + height * zoom * 0.5
+            ))
         }
         return out
     }
 
-    private fun ease(t: Double, from: Complex, to: Complex): Complex {
-        return ZERO
-    }
-
-    /**
-     * Convert a [BufferedImage] of any type, to [BufferedImage] of a
-     * specified type. If the source image is the same type as the target type,
-     * then original image is returned, otherwise new image of the correct type is
-     * created and the content of the source image is copied into the new image.
-     *
-     * @param sourceImage the image to be converted
-     * @param targetType  the desired BufferedImage type
-     * @return a BufferedImage of the specifed target type.
-     * @see BufferedImage
-     */
     private fun convertToType(sourceImage: BufferedImage, targetType: Int): BufferedImage {
         val image: BufferedImage
-
-        // if the source image is already the target type, return the source image
         if (sourceImage.type == targetType) image = sourceImage else {
             image = BufferedImage(
                 sourceImage.width,
