69a36eafdbd7fc656481b172ef0c8d814746f8bb
bnfna <bnfnal@gmail.com>
2021-12-14 16:49:38 +0300

    Добавили в класс MainFrame() обработчика события клика мышки по панели.

--- src/main/kotlin/ru/smak/ui/MainFrame.kt
+++ src/main/kotlin/ru/smak/ui/MainFrame.kt
@@ -9,6 +9,8 @@ import java.awt.Dimension
 import java.awt.Rectangle
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.GroupLayout
 import javax.swing.JFrame
 import kotlin.random.Random
@@ -17,6 +19,7 @@ class MainFrame : JFrame() {
 
     private val fractalPanel: SelectablePanel
     private val plane: CartesianPlane
+    private var frameColorizer: (Double)->Color
 
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -27,6 +30,7 @@ class MainFrame : JFrame() {
         fractalPanel = SelectablePanel(
             FractalPainter(plane, Mandelbrot).apply {
                 colorizer = colorizers[Random.nextInt(colorizers.size)]
+                frameColorizer = colorizer
             }
         ).apply {
             background = Color.WHITE
@@ -38,6 +42,18 @@ class MainFrame : JFrame() {
                 repaint()
             }
         }
+
+        // Если пользователь кликнет по панели с изображением множества Мандельброта, появится окно с изображением множества Жюлиа
+        fractalPanel.addMouseListener(object : MouseAdapter(){
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e?.button == 1) {
+                    with(plane){
+                        val juliaFrame = JuliaFrame(xScr2Crt(e.x), yScr2Crt(e.y), frameColorizer, this)
+                        juliaFrame.setVisible(true);
+                    }
+                }
+            }
+        })
 
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
