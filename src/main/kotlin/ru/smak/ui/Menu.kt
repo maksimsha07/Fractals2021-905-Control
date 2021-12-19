ee75fb3ce714b961bcb850f5e2f19b3be6578596
stasya608 <20012013.n.p@mail.ru>
2021-12-19 18:20:20 +0300

    Кнопки: сохранение и открытие

--- src/main/kotlin/ru/smak/ui/Menu.kt
+++ src/main/kotlin/ru/smak/ui/Menu.kt
@@ -4,6 +4,8 @@ import javax.swing.*
 
 class Menu (val mf: MainFrame): JFrame() {
     var menuBar = JMenuBar()
+    val f1 : JRadioButtonMenuItem = JRadioButtonMenuItem("Множество Мандельброта")
+    val f2 = JRadioButtonMenuItem("Множество Жюли")
 
     init {
         //заполняем меню
@@ -22,6 +24,30 @@ class Menu (val mf: MainFrame): JFrame() {
     }
 
     private fun createFileMenu(): JMenu{
+        //Создание выпадающего меню
+        val fileMenu = JMenu("Файл")
+        //Пункт меню "Открыть"
+        val open = JMenuItem("Открыть")
+        open.addActionListener {
 
+        }
+        //Пункт меню "Сохранить как..."
+        val save = JMenu("Сохранить как...")
+        //Подпункты "Сохранить как..."
+        val format1 = JMenuItem("Собственный формат")
+        format1.addActionListener {
+            f1.doClick()
+        }
+        val format2 = JMenuItem("Изображение")
+        format2.addActionListener {
+        }
+        //Добавление подпунктов в пункты
+        save.add(format1)
+        save.add(format2)
+        fileMenu.add(open)
+        // Добавление разделителя
+        fileMenu.addSeparator()
+        fileMenu.add(save)
+        return fileMenu
     }
 }
