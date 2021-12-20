46614b60787f3a78c1b347fbe6c2142361fbff9a
maksimsha07 <cheshikhin.maksim@gmail.com>
2021-12-20 11:37:56 +0300

    Баги

--- src/main/kotlin/ru/smak/ui/MainFrame.kt
+++ src/main/kotlin/ru/smak/ui/MainFrame.kt
@@ -1,10 +1,10 @@
 package ru.smak.ui
 
 import ru.smak.math.fractals.Mandelbrot
-import ru.smak.video.AnimationFrame
 import ru.smak.ui.painting.CartesianPlane
 import ru.smak.ui.painting.FractalPainter
 import ru.smak.ui.painting.fractals.colorizers
+import ru.smak.video.AnimationFrame
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.event.KeyAdapter
@@ -12,9 +12,10 @@ import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.lang.Math.round
-import java.util.ArrayDeque
+import java.util.*
 import javax.swing.GroupLayout
 import javax.swing.JFrame
+import javax.swing.KeyStroke
 import kotlin.random.Random
 
 class MainFrame : JFrame() {
@@ -23,13 +24,14 @@ class MainFrame : JFrame() {
     private val plane: CartesianPlane
     private var frameColorizer: (Double)->Color    // переменная, в которой будет храниться значение колорайзера для фракталов
     private var juliaFrame: JFrame        // окно, в котором будет отображаться множество Жюлиа
-    private var buffer = ArrayDeque<List<Pair<Double,Double>>>()
+    private var buffer = ArrayDeque<List<Double>>()
 
     init {
+        val menu = Menu()
         defaultCloseOperation = EXIT_ON_CLOSE
         minimumSize = Dimension(600, 400)
         juliaFrame = JFrame()
-        buffer.push(mutableListOf(Pair(-2.0,1.0), Pair(-1.0,1.0)))
+        buffer.push(mutableListOf(-2.0,1.0,-1.0,1.0,50.0))
         plane = CartesianPlane(-2.0, 1.0, -1.0, 1.0)
         fractalPanel = SelectablePanel(
             FractalPainter(plane, Mandelbrot).apply {
@@ -42,23 +44,45 @@ class MainFrame : JFrame() {
                 with(plane){
                     xSegment = Pair(xScr2Crt(r.x), xScr2Crt(r.x+r.width))
                     ySegment = Pair(yScr2Crt(r.y), yScr2Crt(r.y+r.height))
-                    buffer.push(mutableListOf(xSegment,ySegment))
+                    buffer.push(mutableListOf(xSegment.first,xSegment.second,ySegment.first,ySegment.second,Mandelbrot.maxIterations.toDouble()))
                 }
-                var a= Math.abs(plane.ySegment.first-plane.ySegment.second)
-                if (a>=1.0){Mandelbrot.maxIterations = 50
-                }else{
-                    if (a>=0.5&&a<1.0){ Mandelbrot.maxIterations = 1000 - round(a * 900).toInt()
-                    }else {
-                        if (a>=0.2&&a<0.5){ Mandelbrot.maxIterations = 1000 - round(a * 2000).toInt()
+                if(menu.detail.isSelected){
+                    var a= Math.abs(plane.ySegment.first-plane.ySegment.second)
+                    if (a>=1.0){Mandelbrot.maxIterations = 50
+                    }else{
+                        if (a>=0.5&&a<1.0){ Mandelbrot.maxIterations = 1000 - round(a * 900).toInt()
                         }else {
-                            if (a>=0.1&&a<0.2){ Mandelbrot.maxIterations = 1000 - round(a * 4000).toInt()
-                            }else{Mandelbrot.maxIterations = 1000 - round(a * 8000).toInt()}
-                }}}
+                            if (a>=0.2&&a<0.5){ Mandelbrot.maxIterations = 1000 - round(a * 2000).toInt()
+                            }else {
+                                if (a>=0.1&&a<0.2){ Mandelbrot.maxIterations = 1000 - round(a * 4000).toInt()
+                                }else{Mandelbrot.maxIterations = 1000 - round(a * 8000).toInt()}
+                    }}}
+                }
+                else{Mandelbrot.maxIterations = 50}
                 repaint()
             }
         }
+        menu.detail.addMouseListener(object : MouseAdapter(){
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e?.button == 1) {
+                    if(menu.detail.isSelected){
+                        var a= Math.abs(plane.ySegment.first-plane.ySegment.second)
+                        if (a>=1.0){Mandelbrot.maxIterations = 50
+                        }else{
+                            if (a>=0.5&&a<1.0){ Mandelbrot.maxIterations = 1000 - round(a * 900).toInt()
+                            }else {
+                                if (a>=0.2&&a<0.5){ Mandelbrot.maxIterations = 1000 - round(a * 2000).toInt()
+                                }else {
+                                    if (a>=0.1&&a<0.2){ Mandelbrot.maxIterations = 1000 - round(a * 4000).toInt()
+                                    }else{Mandelbrot.maxIterations = 1000 - round(a * 8000).toInt()}
+                                }}}
+                    }
+                    else{Mandelbrot.maxIterations = 50}
+                    repaint()
 
-        fractalPanel.isFocusable = true
+                }
+            }
+        })
         // Если пользователь кликнет по панели с изображением множества Мандельброта, появится окно с изображением множества Жюлиа
         // При чем если уже было открыто одно такое окно, новое окно появится вместо него
         fractalPanel.addMouseListener(object : MouseAdapter(){
@@ -74,39 +98,82 @@ class MainFrame : JFrame() {
             }
         })
 
-        fractalPanel.addKeyListener(object : KeyAdapter(){
-            override fun keyTyped(e: KeyEvent?) {
+          menu.videoBtn.addKeyListener(object : KeyAdapter(){
+            override fun keyPressed(e: KeyEvent?) {
                 super.keyPressed(e)
                 if (e != null)
-                    if (e.keyChar == 'z' || e.keyChar == 'я') {
-                        var segment = buffer.pop()
-                        plane.xSegment = segment[0]
-                        plane.ySegment = segment[1]
+                    if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+                        println("Назад")
+
+                        buffer.pop()
                         if(buffer.size == 0){
-                            buffer.push(mutableListOf(Pair(-2.0,1.0), Pair(-1.0,1.0)))
+                            buffer.push(mutableListOf(-2.0,1.0,-1.0,1.0,50.0))
                         }
+                        var segment = buffer.first
+                        plane.xSegment = Pair(segment[0],segment[1])
+                        plane.ySegment = Pair(segment[2],segment[3])
+                        Mandelbrot.maxIterations = segment[4].toInt()
+                        if(buffer.size == 0){
+                            buffer.push(mutableListOf(-2.0,1.0,-1.0,1.0,50.0))
+                        }
+                        repaint()
                     }
-                repaint()
+            }
+          })
+
+        menu.detail.addKeyListener(object : KeyAdapter(){
+            override fun keyPressed(e: KeyEvent?) {
+                super.keyPressed(e)
+                if (e != null)
+                    if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+                        println("Назад")
+                        buffer.pop()
+                        if(buffer.size == 0){
+                            buffer.push(mutableListOf(-2.0,1.0,-1.0,1.0,50.0))
+                        }
+                        var segment = buffer.pop()
+                        plane.xSegment = Pair(segment[0],segment[1])
+                        plane.ySegment = Pair(segment[2],segment[3])
+                        Mandelbrot.maxIterations = segment[4].toInt()
+                        if(buffer.size == 0){
+                            buffer.push(mutableListOf(-2.0,1.0,-1.0,1.0,50.0))
+                        }
+                        repaint()
+                    }
+            }
+        })
+        val af = AnimationFrame(plane, Mandelbrot, frameColorizer)
+
+        menu.videoBtn.addMouseListener(object :MouseAdapter(){
+            override fun mouseClicked(e: MouseEvent?) {
+                super.mouseClicked(e)
+                if(e != null)
+                    af.apply {
+                        isVisible = true
+                    }
 
             }
         })
 
-        val af = AnimationFrame(plane, Mandelbrot, frameColorizer)
-        af.apply {
-            isVisible = true
-        }
+
 
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(4)
-                    .addComponent(fractalPanel)
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(menu.menuBar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                            .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    )
                     .addGap(4)
             )
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(4)
-                    .addComponent(fractalPanel)
+                    .addComponent(menu.menuBar, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addGap(4)
+                    .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                     .addGap(4)
             )
         }
